41155304c7c65afd043cea9f7c14d27400b7804d
HongYongWoo <bluebox587@gmail.com>
2024-06-17 18:43:56 +0900

    정올 단계별문제 반복제어문2 자가진단 및 형성평가, 연습문제 일부

---
+++ JungOl/StepByStepQuiz/src/f_loop_one/Loop_practice1.java
@@ -0,0 +1,11 @@
+package f_loop_one;
+
+public class Loop_practice1 {
+    public static void main(String[] args) {
+        for(char x = 'A'; x<= 'Z' ; x++) {
+            System.out.printf("%c",x);
+        }
+    }
+}
+
+//Quiz.알파벳 'A'부터 'Z'까지 출력하는 프로그램을 작성하시오.
---
+++ JungOl/StepByStepQuiz/src/g_loop_two/loop2_Diagnosis5.java
@@ -0,0 +1,29 @@
+package g_loop_two;
+import  java.io.*;
+import java.util.StringTokenizer;
+
+public class loop2_Diagnosis5 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer((br.readLine()));
+
+        int num =0;
+        int mul3 =0, mul5=0;
+
+
+        for(int n=0 ; n<10 ;n++) {
+            num = Integer.parseInt(st.nextToken());
+            if(num %3 ==0)
+                mul3++;
+            if(num %5 ==0)
+                mul5++;
+
+        }
+
+        System.out.printf("Multiples of 3 : %d\nMultiples of 5 : %d\n",mul3, mul5);
+
+        br.close();
+    }
+}
+
+//Quiz. 10개의 정수를 입력받아 3의 배수의 개수와 5의 배수의 개수를 각각 출력하는 프로그램을 작성하시오
