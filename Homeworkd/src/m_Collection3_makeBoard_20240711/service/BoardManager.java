1e78c923974773a8c10f12bf0f5b04c8a8179f8e
HongYongWoo <bluebox587@gmail.com>
2024-07-19 14:42:52 +0900

    UML Class 다이어 그램 추가

--- Homeworkd/src/m_Collection3_makeBoard_20240711/service/BoardManager.java
+++ Homeworkd/src/m_Collection3_makeBoard_20240711/service/BoardManager.java
@@ -14,33 +14,4 @@ public class BoardManager extends ServiceMenu{
   private BoardManager() {
   }
 
-  @Override
-  public void insertboard(Board board) {
-    super.insertboard(board);
-  }
-
-  @Override
-  public void deleteBoard(int bno) {
-    super.deleteBoard(bno);
-  }
-
-  @Override
-  public void clearBoard() {
-    super.clearBoard();
-  }
-
-  @Override
-  public Board searchBoard(int no) {
-    return super.searchBoard(no);
-  }
-
-  @Override
-  public ArrayList<Board> listAllBoard() {
-    return super.listAllBoard();
-  }
-
-  @Override
-  public void updateBoard(int no, Board board) {
-    super.updateBoard(no, board);
-  }
 }
